38c847b7c3e851a89e5851a58fe11fb6539a95bf
DongpingZ <cadongllas@gmail.com>
2021-12-13 01:44:53 +0800

    read the signal

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ eur = test.PriceRecord('EURUSD') #调用test文件里面的方法和类，前面
 column = ['date', 'week', 'type', 'direction', 'entry', 'cut', 'first', 'second']
 TradeSignal = pd.read_csv('data/TradeSignal(1).csv', names = column)
 TradeSignal = signal.changeDataFormate (TradeSignal)
-print( TradeSignal)
+# print( TradeSignal)
+
+
+print (TradeSignal.loc[3])
+print (TradeSignal.loc[3]['type'])
+
+for i in range (1, len (TradeSignal)):
+# for i in range (3, 100):
+    # str =TradeSignal.loc[i]['type']
+    # print (str)
+    if TradeSignal.loc[i]['type'] == 'EURUSD':
+        print (i)
+        print(TradeSignal.loc[i])
+

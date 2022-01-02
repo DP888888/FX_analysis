8a069e8c495207e1f93b2f6c65eccd999de26e4d
DongpingZ <cadongllas@gmail.com>
2022-01-02 16:25:47 +0800

    calc trace

--- main.py
+++ main.py
@@ -11,56 +11,82 @@ TradeSignal = pd.read_csv('data/TradeSignal(1).csv', names = column)
 TradeSignal = signal.changeDataFormate (TradeSignal)
 # print( TradeSignal)
 
-def FindPos (nowPrice, SignalArray, long):
-    BeginPrice = float (nowPrice.OPEN)
+def Between (strCurPrice, CandleRecord):
+    CurPrice = float ( strCurPrice)
+    Min = min (float (CandleRecord.OPEN), float (CandleRecord.CLOSE))
+    Max = max (float (CandleRecord.OPEN), float (CandleRecord.CLOSE))
+    return CurPrice >= Min and CurPrice <= Max
+
+
+def FindPos (CandleRecord, SignalArray, long):
+    Keys = list(SignalArray.keys())
+    Values = list(SignalArray.values())
+
     if long:
-        # Long, from top to down
-        if BeginPrice > list (SignalArray.values())[0]:
-            return -1
+        if CandleRecord.OPEN <= CandleRecord.CLOSE:
+            #up candle
+            # find the highest match position, find from high to low
+            for i in range(len(Values)):
+                if Between (Values[i], CandleRecord):
+                    return Keys[i]
+        else:
+            # down candle
+            # find the lowest match position, find from low to high
+            for i in range(len(Values) - 1, 0, -1):
+                if Between (Values[i], CandleRecord):
+                    return Keys[i]
     else:
-
-        Keys = list (SignalArray.keys ())
-        Values = list (SignalArray.values ())
-        for i in (len (SignalArray) - 1, 0, -1):
-            if BeginPrice >= Values[i]:
-                return Keys[i]
-        assert  (0)
-
+        # short
+        if CandleRecord.OPEN <= CandleRecord.CLOSE:
+            #up candle
+            # find the highest match position, find from high to low
+            for i in range(len(Values) - 1, 0, -1):
+                if Between (Values[i], CandleRecord):
+                    return Keys[i]
+        else:
+            # down candle
+            # find the lowest match position, find from low to high
+            for i in range(len(Values)):
+                if Between (Values[i], CandleRecord):
+                    return Keys[i]
     return -1
 
 
+
+
 def FindTraceFromPriceAndSignal(TodayPriceRecord, SignalArray, long):
-    print (long)
-    curPos = FindPos (TodayPriceRecord.iloc[0], SignalArray, long)
-    print ('curPos  ', curPos)
+    # print (long)
+    curPos = -1
     i = 0
-    # for index, row in TodayPriceRecord.iterrows():
-    #     if curPos == -1:
-    #         curPos = FindPos(TodayPriceRecord)
-    return []
+    ret = []
+    for index, row in TodayPriceRecord.iterrows():
+        nextPos = FindPos(row, SignalArray, long)
+        if nextPos != -1 and nextPos != curPos:
+            curPos = nextPos
+            ret.append (curPos)
+    # print (SignalArray)
+    return ret
 
 
 def work (typeName):
     price = test.PriceRecord (typeName)
-    price.printM1()
+    # price.printM1()
 
-    for i in range (1, 2):
-    # for i in range (1, len (TradeSignal)):
-        # str =TradeSignal.loc[i]['type']
-        # print (str)
-        if TradeSignal.loc[i]['type'] == typeName:
-            print (i)
+    for i in range (1, 20):
+        if TradeSignal.loc[i]['type'] == typeName :
+            print ('=========== ', i)
             TodayPriceRecord = price.findPriceGivenDate (TradeSignal.loc[i]['date'] )
-            print (TodayPriceRecord)
-            print(TradeSignal.loc[i])
+            # print (TodayPriceRecord)
+            # print(TradeSignal.loc[i])
             SignalArray = {}
             SignalArray[0] = float (TradeSignal.loc[i].Second)
             SignalArray[1] = float (TradeSignal.loc[i].First)
             SignalArray[2] = float (TradeSignal.loc[i].entry)
             SignalArray[3] = float (TradeSignal.loc[i].cut)
-            print (SignalArray)
-            trace = FindTraceFromPriceAndSignal (TodayPriceRecord, SignalArray, TradeSignal.loc[i].direction == 'short')
+            # print (SignalArray)
+            trace = FindTraceFromPriceAndSignal (TodayPriceRecord, SignalArray, TradeSignal.loc[i].direction == 'long')
             print (trace)
+            # break
 
 
 
--- test.py
+++ test.py
@@ -17,19 +17,16 @@ def StrToDateTime(input):
 
 def FindPosGivenDateTime(priceDate, SignalDate):
     # priceDate = pd.DataFrame.as_matrix (df_priceDate)
+    # print (priceDate[1000:2000])
     # print (priceDate)
-    print ('begin find !')
+    # print ('begin find !', SignalDate)
     # print (priceDate.loc[4])
     l = 1
     r = len (priceDate)
     ans = -1
-    while (l < r):
+    while (l <= r):
         mid = (l + r) // 2
-        # print (mid, priceDate.loc [mid])
-        # str = priceDate.loc [mid]['SignalDate'] + ' ' + priceDate.loc [mid]['TIME']
-        # dateFormatter = "%Y.%m.%d %H:%M:%S"
-        # t = datetime.strptime(str, dateFormatter)
-        # print (t)
+        # print (priceDate.loc[mid])
 
         if SignalDate == StrToDateTime (priceDate.loc[mid]):
             ans = mid
@@ -39,8 +36,9 @@ def FindPosGivenDateTime(priceDate, SignalDate):
         if SignalDate > StrToDateTime (priceDate.loc[mid]):
             l = mid + 1
 
-    print ('finish find ', ans)
-    print (SignalDate, priceDate.loc[ans])
+    # print ('finish find ', ans)
+    assert (ans != -1) #must find price record
+    # print (SignalDate, priceDate.loc[ans])
     return ans
 
 
@@ -58,7 +56,7 @@ class PriceRecord:
     def findPriceGivenDate (self, SignalDate):
         NewDate = ''
         num = 0
-        print (SignalDate, 'begin find ..')
+        # print (SignalDate, 'begin find ..')
 
         dateFormatter = "%Y/%m/%d"
         signalDate = datetime.datetime.strptime(SignalDate, dateFormatter)
@@ -70,5 +68,5 @@ class PriceRecord:
 
         BeginPos = FindPosGivenDateTime (self.M1price, signalDate + datetime.timedelta(hours=9))
         EndPos = FindPosGivenDateTime (self.M1price, signalDate + datetime.timedelta(hours=13))
-        print ('finish ')
+        # print ('finish ')
         return self.M1price[BeginPos: EndPos]

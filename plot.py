9741a799a8b463dc10a139385a52c0998ab4cf7a
cadongllas <cadongllas@gmail.com>
2022-01-25 13:56:51 +0800

    can skip holiday now

--- plot.py
+++ plot.py
@@ -1,10 +1,13 @@
 import test
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.ticker as ticker
+from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from mplfinance.original_flavor import candlestick_ohlc
 import matplotlib.dates as mdates
+
+
 
 def work (typeName):
     price = test.PriceRecord (typeName)
@@ -20,26 +23,54 @@ def work (typeName):
     df['OPEN'] = df['OPEN'].astype(float)
 
     print(df)
+    print (' ===')
+    date_tickers = df.DATE.values
+    # print (df.values)
+    weekday_quotes = [tuple([i] + list(quote[1:])) for i, quote in enumerate(df.values)]
+    # print (weekday_quotes)
+    print (date_tickers)
+
+
+    # exit()
 
     df['DATE'] = df['DATE'].apply(mdates.date2num)
     fig, ax = plt.subplots()
     dpi = 300
     fig.set_dpi(dpi)
 
+    def format_date(x, pos=None):
+        if x < 0 or x > len(date_tickers) - 1:
+            return ''
+        ret = date_tickers[int(x)]
+        t = str (ret)
+        l = t.find ('-')
+        r = t.rfind (':')
+        t = t[l + 1:r]
+        pp = t.find ('T')
+        t = t[:pp] + ' ' + t[pp + 1: ]
+        print (t)
+        return t
+
+
     # plotting the data
     # candlestick_ohlc(ax, df.values, width=0.015, colorup='green', colordown='red')
-    candlestick_ohlc(ax, df.values, width=0.015, colorup='green', colordown='red', alpha=0.8)
-    ax.xaxis_date()
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(160))
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+
+    candlestick_ohlc(ax, weekday_quotes, width=0.015, colorup='green', colordown='red', alpha=0.8)
+
+
+    # ax.xaxis_date()
+    # ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
     # ax.xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d %H:%M'))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
     if typeName == 'GBPJPY':
         ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     else:
         ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
     plt.grid()
-    plt.xticks(rotation=45)
+    # plt.xticks(rotation=45)
     # fig.set_size_inches(1920 / dpi, 1980 / dpi)
     plt.show()
 
